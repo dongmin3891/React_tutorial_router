498b384e36a34bf18f34085310733311b373a188
dongmin <rlaehdals753@naver.com>
2021-02-09 21:08:38 +0900

    Link: 누르면 다른 주소로 이동시키기

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 
 const App = () => {
   return (
     <div>
-      {/* <Route path="/" component={Home} />
-      <Route path="/about" component={About} /> 
-       
-        /about 경로로 들어가면, 예상과 다르게 두 컴포넌트가 모두 보여집니다!
-        이는 /about 경로가 / 규칙과도 일치하기 때문에 발생한 현상인데요, 
-        이를 고치기 위해선 Home 을 위한 라우트에 exact 라는 props 를 true 로 설정하시면 됩니다.
-      */}
-
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-      {/* 이렇게 하면 경로가 완벽히 똑같을때만 컴포넌트를 
-          보여주게 되어 이슈가 해결됩니다. */}
     </div>
   );
 };
 
 export default App;
 
-/* 사용자가 요청하는 주소에 따라 다른 컴포넌트를 보여줘보겠습니다. 
-이 작업을 할 때에는 Route 라는 컴포넌트를 사용합니다.
+/* 
+Link 컴포넌트는 클릭하면 다른 주소로 이동시키는 컴포넌트입니다.
+ 리액트 라우터를 사용할땐 일반 <a href="...">...</a> 태그를 사용하시면 안됩니다. 
+ 만약에 하신다면 onClick 에 e.preventDefault() 를 호출하고 
+ 따로 자바스크립트로 주소를 변환시켜주어야 합니다.
 
-사용 방식은 다음과 같습니다:
-
-<Route path="주소규칙" component={보여주고싶은 컴포넌트}>
-
-한번 App.js 에서 기존 코드들을 날리고, Route 들을 렌더링해주겠습니다. */
+그 대신에 Link 라는 컴포넌트를 사용해야하는데요, 
+그 이유는 a 태그의 기본적인 속성은 페이지를 이동시키면서,
+페이지를 아예 새로 불러오게됩니다. 
+그렇게 되면서 우리 리액트 앱이 지니고있는 상태들도 초기화되고, 
+렌더링된 컴포넌트도 모두 사라지고 새로 렌더링을 하게됩니다. 
+그렇기 때문에 a 태그 대신에 Link 컴포넌트를 사용하는데요,
+ 이 컴포넌트는 HTML5 History API 를 사용하여 
+ 브라우저의 주소만 바꿀뿐 페이지를 새로 불러오지는 않습니다.
+*/
