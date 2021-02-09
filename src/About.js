dd4cef4d617fd5627c5b44bb1f2260fea16a4644
dongmin <rlaehdals753@naver.com>
2021-02-09 20:54:47 +0900

    페이지 만들기

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트랍니다.</p>
+    </div>
+  );
+};
+
+export default About;
+
+//웹 사이트의 소개를 보여주는 About페이지
