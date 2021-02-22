b43e5891d3dfa044ca4ed04eaab84cf9f112005d
dongmin <rlaehdals753@naver.com>
2021-02-22 20:56:14 +0900

    리액트 프로젝트에 리덕스 적용하기

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import About from "./About";
-import Home from "./Home";
-import Profiles from "./Profiles";
-import HistorySample from "./HistorySample";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./modules";
 
-const App = () => {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-        <li>
-          <Link to="/profiles">프로필 목록</Link>
-        </li>
-        <li>
-          <Link to="/history">예제</Link>
-        </li>
-      </ul>
-      <hr />
-      <Switch>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/profiles" component={Profiles} />
-        <Route path="/history" component={HistorySample} />
-        <Route
-          // path 를 따로 정의하지 않으면 모든 상황에 렌더링됨
-          render={({ location }) => (
-            <div>
-              <h2>이 페이지는 존재하지 않습니다:</h2>
-              <p>{location.pathname}</p>
-            </div>
-          )}
-        />
-      </Switch>
-    </div>
-  );
-};
+const store = createStore(rootReducer); // 스토어를 만듭니다.
 
-export default App;
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
