30b4f71783c8c45dfeb4e0265e5d1169fd33cd6e
dongmin <rlaehdals753@naver.com>
2021-03-01 19:54:04 +0900

    미들웨어 만들어보고 이해하기 - 1

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
-import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자 도구
+import myLogger from "./middlewares/myLogger";
 
-const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
-// composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 ReactDOM.render(
   <Provider store={store}>
