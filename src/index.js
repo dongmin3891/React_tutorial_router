af79cfcb0d65c62c2fb34f9543e522e7fafc2807
dongmin <rlaehdals753@naver.com>
2021-03-05 16:07:37 +0900

    redux-thunk로 프로미스 다루기

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import rootReducer from "./modules";
-import logger from "redux-logger";
-import { composeWithDevTools } from "redux-devtools-extension";
-import ReduxThunk from "redux-thunk";
+import { combineReducers } from "redux";
+import counter from "./counter";
+import posts from "./posts";
 
-const store = createStore(
-  rootReducer,
-  // logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
-  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
-); // 여러개의 미들웨어를 적용 할 수 있습니다.
+const rootReducer = combineReducers({ counter, posts });
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
-
-serviceWorker.unregister();
+export default rootReducer;
